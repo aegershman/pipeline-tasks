230909e88f9413556859fc18789280635b90da13
Aaron Gershman <aaron.gershman@cerner.com>
2017-11-13 09:25:30 -0600

    GWXOPS-304

---
+++ run-maven-failsafe-cache/task.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+#
+# All UPERCASE variables are provided externally from this script
+
+set -eu
+set -o pipefail
+
+cd project
+
+args=""
+[ -n "$MAVEN_PROJECTS" ] && args="$args --projects $MAVEN_PROJECTS"
+[ -n "$MAVEN_REPO_MIRROR" ] && args="$args -Drepository.url=$MAVEN_REPO_MIRROR";
+[ -n "$MAVEN_REPO_USERNAME" ] && args="$args -Drepository.username=$MAVEN_REPO_USERNAME";
+[ -n "$MAVEN_REPO_PASSWORD" ] && args="$args -Drepository.password=$MAVEN_REPO_PASSWORD";
+[ "true" = "$MAVEN_REPO_CACHE_ENABLE" ] && args="$args -Dmaven.repo.local=$PWD/.m2repository"
+
+./mvnw failsafe:integration-test $args
+
+cd ..
