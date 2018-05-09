47729ee8228f1c01371a7db24a3cab85d091ce00
bk016677 <bryan.kelly@cerner.com>
2018-05-09 15:34:30 -0500

    Removed local directive on variable declarations.

--- cf-command/task.sh
+++ cf-command/task.sh
@@ -7,21 +7,21 @@ set -o pipefail
 exec 3>&1 # make stdout available as fd 3 for the result
 exec 1>&2 # redirect all output to stderr for logging
 
-local api_endpoint=${api:?api_endpoint null or not set}
-local cf_user=${username:?username null or not set}
-local cf_pass=${password:?password null or not set}
-local skip_ssl_validation=${skip_cert_check:-false}
+api_endpoint=${api:?api_endpoint null or not set}
+cf_user=${username:?username null or not set}
+cf_pass=${password:?password null or not set}
+skip_ssl_validation=${skip_cert_check:-false}
 
-local args=("$api_endpoint")
+args=("$api_endpoint")
 [ "$skip_ssl_validation" = "true" ] && args+=(--skip-ssl-validation)
 
 cf api "${args[@]}"
 cf auth "$cf_user" "$cf_pass"
 
-local org=${org:?org null or not set}
-local space=${space:-}
+org=${org:?org null or not set}
+space=${space:-}
 
-local args=(-o "$org")
+args=(-o "$org")
 [ -n "$space" ] && args+=(-s "$space")
 
 cf target "${args[@]}"
